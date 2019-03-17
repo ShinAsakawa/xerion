da9043e633213e7e335ac8a4b2bdc17cfc619218
Shin Asakawa <ShinAsakawa@users.noreply.github.com>
2019-03-18 08:19:53 +0900

    uploaded

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='xerion',
+      version='0.3',
+      packages=['xerion'],
+      description='xerion dataset management',
+      author='Shin Asakawa',
+      author_email='asakawa@ieee.org',
+      url='https://github.com/ShinAsakawa/wbai_aphasia',
+      download_url='https://github.com/ShinAsakawa/xerion/',
+      license='Apache 2.0',
+      )
